025f5105c10400b00c484446507442feb231761f
Daniel Morcuende <dmorcuen@ucm.es>
2021-04-30 15:30:26 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='hipyi',
+    version='v0.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Daniel Morcuende',
+    author_email='dmorcuende@gmail.com',
+    description='Test pypi deply'
+)
