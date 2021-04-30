e2f3b4f1745c88e110eedd4bc506e58d7bfa0ee7
Daniel Morcuende <dmorcuen@ucm.es>
2021-04-30 15:38:27 +0200

    versioning

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
+import os
 
 setup(
     name='hipyi',
+    use_scm_version={"write_to": os.path.join("hipypi", "_version.py")},
     version='v0.0.0',
     packages=[''],
     url='',
